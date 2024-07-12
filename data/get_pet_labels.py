3c65c9cd824f3877c91bcc83f7999c879262e9c9
Eddie Otudor <edbeeotudor@gmail>
2024-07-12 15:33:27 +0000

    Add necessary details

--- data/get_pet_labels.py
+++ data/get_pet_labels.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: Eddie Otudor
+# DATE CREATED: 12/07/2024                                 
+# REVISED DATE: 12/07/2024
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
