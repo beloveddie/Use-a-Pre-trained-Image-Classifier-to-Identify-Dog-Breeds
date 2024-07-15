ea7b02c9d2c70b2d0f575b8fc53a7de63ed3e2e5
Eddie Otudor <edbeeotudor@gmail.com>
2024-07-15 00:04:55 +0000

    Add metadata

--- data/print_results.py
+++ data/print_results.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/print_results.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:
-# REVISED DATE: 
+# PROGRAMMER: Eddie Otudor
+# DATE CREATED: 15/07/2024
+# REVISED DATE: 15/07/2024
 # PURPOSE: Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
 #          should also allow the user to be able to print out cases of misclassified
