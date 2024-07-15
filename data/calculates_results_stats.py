f9772ccf895c9cedf9550732a489ba937a33d898
Eddie Otudor <edbeeotudor@gmail.com>
2024-07-15 00:03:05 +0000

    Add metadata

--- data/calculates_results_stats.py
+++ data/calculates_results_stats.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: Eddie Otudor
+# DATE CREATED: 15/07/2024                              
+# REVISED DATE: 15/07/2024
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
 #          architecture to classify the images. This function will use the 
